54ef96667f5e46eaed5c4e6bdddaacf485c4533f
thinkwithabs <twithabs@gmail.com>
2022-06-05 00:17:37 +0600

    database and env modify

--- Charidik/settings.py
+++ Charidik/settings.py
@@ -1,5 +1,6 @@
 
 
+
 from pathlib import Path
 import os
 import sys
@@ -29,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # DEBUG = True
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 
