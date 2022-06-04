57f929311433f8fcea06be5d243ac52423e959e5
thinkwithabs <twithabs@gmail.com>
2022-06-05 01:42:25 +0600

    new code added

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn Charidik.wsgi --log-file=- 
--- utils/util.py
+++ utils/util.py
@@ -5,6 +5,5 @@ class Util:
     @staticmethod
     def modify_input_for_multiple_files( image):
         dict = {}
-        # dict['property_id'] = property_id
         dict['image'] = image
         return dict
